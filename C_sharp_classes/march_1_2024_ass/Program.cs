4ff017b45455c0d8f10d8900af7932fe0f187c83
thigam <matthewthiga72@gmail.com>
2024-03-01 16:11:50 +0100

    First push for file

---
+++ C_sharp_classes/march_1_2024_ass/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Assignment_1
+{
+    internal class Program{
+    public static void Main(){
+        Student Anya = new Student();
+        Anya.FirstName = "Anya";
+        Console.WriteLine(Anya.FirstName);
+    }
+
+    public class Student{
+        private string firstName;
+        private string lastName;
+        private string neptunCode;
+        private int age;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName, string neptunCode, int age)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                throw new ArgumentException("First name or last name must be filled");
+            }
+            if (age < 1 || age > 150)
+            {
+                throw new ArgumentException("Age must be between 1 and 150");
+            }
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.neptunCode = neptunCode;
+            this.age = age;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            set{
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("First name or last name must be filled");
+                }
+
+                this.firstName = value;
+            }
+        }
+    }
+    }
+}
